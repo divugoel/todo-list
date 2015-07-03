1f761be793b89868a3d32c6b5d4e05e22a61bd63
Prashant Chaudhary <101.prashant@gmail.com>
2015-07-03 15:38:34 +0530

    switched to sqlite database

--- src/Main.java
+++ src/Main.java
@@ -3,27 +3,37 @@ import java.sql.*;
 public class Main {
     public static void main(String[] args) {
         try {
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            Class.forName("org.sqlite.JDBC").newInstance();
         } catch (Exception e) {
-            System.out.println("Mysql driver not found");
+            System.out.println("Slite driver not found");
             return;
         }
 
-        String url = "jdbc:mysql://localhost/todo_list";
-        String username = "codekamp";
-        String password = "codekamp.in";
-
+        String url = "jdbc:sqlite:todo_list.db";
 
         try {
-            Connection conn  = DriverManager.getConnection(url, username, password);
+            Connection conn  = DriverManager.getConnection(url);
             Statement stmt = conn.createStatement();
-            ResultSet result = stmt.executeQuery("SELECT * from list WHERE id = 1");
+
+            String query = "CREATE TABLE `list` ( `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, `title` TEXT NOT NULL, `description` TEXT );";
+            stmt.executeUpdate(query);
+            
+            String title = "Another task task";
+            String description = "fladjg ahdjfoadf oahsdfoadf oaodshfoahd";
+
+            query = "INSERT INTO list (title, description) values ('" + title + "', '" + description + "')";
+            System.out.println(query);
+
+            stmt.executeUpdate(query);
+
+
+            ResultSet result = stmt.executeQuery("SELECT * from list");
 
             while(result.next()) {
                 System.out.println("id: " + result.getInt("id"));
                 System.out.println("title: " + result.getString("title"));
                 System.out.println("description: " + result.getString("description"));
-                System.out.println("done: " + result.getBoolean("done"));
+//                System.out.println("done: " + result.getBoolean("done"));
                 System.out.println("\n\n");
             }
         } catch (SQLException e) {
